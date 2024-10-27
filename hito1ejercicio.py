7dc4e8ae45c05fb5a439c4ac86c1a252a6dca5bf
SergioArroyo1 <sergio.gonzalezarroyo24@campusfp.es>
2024-10-27 21:56:23 +0100

    Add files via upload

--- hito1ejercicio.py
+++ hito1ejercicio.py
@@ -1,11 +1,13 @@
-'''A través de un menú solicitaremos alusuario que tipo de figura quiere mostrar (1-Cuadrado|2-Rectángulo), si la opción no escorrecta, se mostrará mensaje de error y se volverá a solicitar hasta que se correcta.  Si ha seleccionada un cuadrado, pediremos su lado y mostraremos la figura, su área yperímetroSi ha seleccionada un rectángulo, pediremos base y altura y mostraremos la figura, su área y perímetr'''
+'''A través de un menú solicitaremos alusuario que tipo de figura quiere mostrar (1-Cuadrado|2-Rectángulo), si la opción no escorrecta, se mostrará mensaje de error y se volverá a solicitar hasta que se correcta.  Si ha seleccionada un cuadrado, pediremos su lado y mostraremos la figura, su área yperímetroSi ha seleccionada un rectángulo, pediremos base y altura y mostraremos la figura, su área y perímetro'''
 
 
 menu=""
 
+#Utilizamos un bucle while para mostrar el menu
 
 while True:
     menu = int(input("Mostrar menu : 1-Cuadrado y 2-Rectangulo: "))
+#Utilizamos condiciones para saber lo que sucede en cada uno de los casos.
     if menu == 1:
         lado = int(input("Dime el lado del cuadrado: "))
         print(f"EL area del cuadrado es {lado * lado }")
@@ -19,7 +21,9 @@ while True:
         print (f"El area del perimetro es {base*2 + altura*2}")
         for i in range (altura):
             print(f"*"*base)
+#En la ultima condion añadimos al break al ingresar el numero tres que nos saca del bucle
     elif menu == 3:
-        break 
+        break
+#Si el numero es distinto a todos los de las condiciones no indica que hay un error y que ingrese otro numero
     else :
         print ("Error introduce un numero valido: ")
